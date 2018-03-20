79deae63822fff7b5407672f56762df530bc9e2c
milk <291509722@qq.com>
2018-03-20 20:40:16 +0800

    update

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import abort, render_template, jsonify
+from flask import abort, render_template, jsonify, url_for
 
 from . import main
 from ..models.auth import User
@@ -7,23 +7,14 @@ from ..models.auth import User
 @main.route('/', methods=["GET"])
 def index():
     user = User.query.all()
-    return render_template("test.html")
+    # return render_template("test.html")
 
 
-@main.route("/test", methods=["GET", "POST"])
-def test():
-    table = dict()
-    table["header"] = ["编号", "姓名", "密码"]
-    table["data"] = []
+@main.route("/test/", defaults={'page': 1})
+@main.route("/test/<int:page>/", methods=["GET", "POST"])
+def test(page=1):
+    theaders = ["id", "name", "password_hash"]
     pagination = User.query.order_by(User.id.desc()).paginate(
-        10, per_page=3, error_out=False
+        page, per_page=5, error_out=False
     )
-    for u in pagination.items:
-        table["data"].append({"id": u.id, "name": u.name, "password": u.password_hash})
-    table['has_prev'] = pagination.has_prev
-    table['has_next'] = pagination.has_next
-    table['pages'] = list(pagination.iter_pages())
-    table['page'] = pagination.page
-    data = dict()
-    data["table"] = table
-    return jsonify(data)
+    return render_template('test.html', theaders=theaders, pagination=pagination, url='main.test')
