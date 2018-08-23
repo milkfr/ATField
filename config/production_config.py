c484b31a4220cfd8423a1f5bce9631d9a4d2945f
milkfr <291509722@qq.com>
2018-08-23 19:42:22 +0800

    delete all

--- config/production_config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from config.base_config import Config, basedir
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
-                             "sqlite:///" + os.path.join(basedir, 'data.sqlite')
